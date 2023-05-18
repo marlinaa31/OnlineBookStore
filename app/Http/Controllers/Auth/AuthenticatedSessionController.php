b8f0e69645aca0dc6cff1dadf365715c8a439b7e
marlinaa31 <marlinausman091@gmail.com>
2023-05-18 23:45:52 +0700

    feat: add multiuser

--- app/Http/Controllers/Auth/AuthenticatedSessionController.php
+++ app/Http/Controllers/Auth/AuthenticatedSessionController.php
@@ -29,6 +29,10 @@ class AuthenticatedSessionController extends Controller
 
         $request->session()->regenerate();
 
+        if(Auth::user()->role == 'admin'){
+            return redirect()->intended('/admin');
+        }
+
         return redirect()->intended(RouteServiceProvider::HOME);
     }
 
--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AdminController;
 
 /*
 |--------------------------------------------------------------------------
@@ -150,11 +151,11 @@ Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+    Route::middleware(['checkRole:admin'])->group(function () {
+        // Dashboard
+        Route::get('/admin', [AdminController::class, 'adminDashboard'])->name('admin');
 });
-
-Route::get('/admin', function () {
-    return view('admin');
-})->name('admin');
+});
 
 Route::get('/category', function () {
     return view('category');
